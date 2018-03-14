74e47943940ecb9c28df8233202a2a7bb6a5870a
Will LeVine <levinwil2@gmail.com>
2018-03-14 16:45:25 -0500

    pipeline v1

--- functions/analytical.py
+++ functions/analytical.py
@@ -1,89 +1,49 @@
+from skimage import exposure
+import cv2
 import numpy as np
 
 
-def has_white_background(image):
+def basis_map(image):
     '''
-    determines if an input image has a white background
+    maps each pixel of an image to a set of features
 
     parameters
     __________
-    image : np.array
-        the raw image
+    image : np.array (128 x 128)
+        the input image
 
     returns
     __________
-    has_white_background : boolean
-        true if the image has a white background, false otherwise
+    np.array (128 x 128 x 9)
+        the image features
     '''
-    image = np.array(image) * 255
-    rgb_sums = np.sum(image, axis=2)
-    if (rgb_sums > 620).sum() >= 14:
-        return True
-    else:
-        return False
-
-
-def is_black_white(image):
-    '''
-    determines if an input image is on a black/white scale
-
-    parameters
-    __________
-    image : np.array
-        the raw image
-
-    returns
-    __________
-    is_black_white : boolean
-        true if the image is on a black and white scale, false otherwise
-    '''
-    image = np.array(image) * 255
-    R = [np.mean(image[:, :, 0])]
-    G = [np.mean(image[:, :, 1])]
-    B = [np.mean(image[:, :, 2])]
-    if np.std([R, G, B]) < .2:
-        return True
-    else:
-        return False
-
-
-def has_purple_foreground(image):
-    '''
-    determines if an input image has a purple foreground
-
-    parameters
-    __________
-    image : np.array
-        the raw image
-
-    returns
-    __________
-    boolean
-        true if the image has a purple foreground, false otherwise
-    '''
-    image = np.array(image) * 255
-    if np.mean(image[:, :, 1]) < 178:
-        return True
-    else:
-        return False
-
-
-def is_sparse(image):
-    '''
-    determines if an input image is sparse or not
-
-    parameters
-    __________
-    image : np.array
-        the raw image
-
-    returns
-    __________
-    is_sparse : boolean
-        true if the image is sparse, false otherwise
-    '''
-    image = np.array(image) * 255
-    if (np.mean(image[:, :, 2]) < 114):
-        return True
-    else:
-        return False
+    new_image = np.zeros((image.shape[0], image.shape[1], 9))
+    mean = np.mean(image)
+    std = np.std(image)
+    bilateral = cv2.bilateralFilter(
+        np.mean(image * 255, axis=2).astype("uint8"), 2, 75, 75) / 255.0
+    p2, p98 = np.percentile(image, (2, 98))
+    img_rescale = np.mean(exposure.rescale_intensity(
+        image, in_range=(p2, p98)), axis=2)
+    equalize_hist = np.mean(exposure.equalize_adapthist(
+        image, clip_limit=0.03), axis=2)
+    dilate = np.mean(cv2.dilate(image, np.ones((5, 5)), iterations=2), axis=2)
+    for i in range(len(image)):
+        for j in range(len(image)):
+            new_image[i][j][0] = image[i][j][0]
+            new_image[i][j][1] = image[i][j][1]
+            new_image[i][j][2] = image[i][j][2]
+            new_image[i][j][3] = (np.mean(image[i][j]) - mean) / std
+            new_image[i][j][4] = bilateral[i][j]
+            new_image[i][j][5] = img_rescale[i][j]
+            new_image[i][j][6] = equalize_hist[i][j]
+            new_image[i][j][7] = dilate[i][j]
+    for i in range(len(image)):
+        for j in range(len(image)):
+            lower_i = max(i - 1, 0)
+            upper_i = min(i + 1, image.shape[0] - 1)
+            lower_j = max(j - 1, 0)
+            upper_j = min(j + 1, image.shape[1] - 1)
+            new_image[i][j][8] = np.mean([np.linalg.norm(new_image[i][j] - new_image[neighbor_i][neighbor_j])
+                                          for neighbor_i in range(lower_i, upper_i + 1) for neighbor_j in range(lower_j, upper_j + 1)])
+    return np.nan_to_num(new_image)
--- functions/main.py
+++ functions/main.py
@@ -1,23 +1,37 @@
 from analytical import *
+from predict import *
+from computer_vision import *
+from clustering import *
 
 
-def predict(X):
-    predictions = []
-    for image in X:
-        # if the image is sparse
-        if is_sparse(image):
-            predictions.append(otsu(image, is_sparse=True))
-        # if the image is dense
+def predict(image):
+    # if the image is sparse
+    if is_sparse(image):
+        print 'sparse'
+        features = basis_map(image)
+        lda_output = lda_predict(features)
+        return non_max_component_suppression(
+            lda_output, min_area=5, percent=99)
+    # if the image is dense
+    else:
+        # if the image is on a black/white scale
+        if is_black_white(image):
+            raise ValueError("not yet implemented")
+        # if the image is on a color scale
         else:
-            # if the image is on a black/white scale
-            if is_black_white(image):
-                raise ValueError("not yet implemented")
-            # if the image is on a color scale
-            else:
-                # if the image has a white background
-                if has_white_background(image):
-                    predictions.append(1 - otsu(image, is_sparse=False))
-                # is the image has a purple background
+            # if the image has a white background
+            if has_white_background(image):
+                if (has_purple_foreground(image)):
+                    features = basis_map(1 - image)
+                    svr_output = lr_predict(features)
+                    return non_max_component_suppression(
+                        (svr_output > .77) * svr_output, min_area=10, percent=95)
                 else:
-                    raise ValueError("not yet implemented")
+                    features = basis_map(1 - image)
+                    lr_output = lr_predict(features)
+                    return non_max_component_suppression(
+                        lr_output, min_area=5, percent=95)
+            # is the image has a purple background
+            else:
+                raise ValueError("not yet implemented")
     return predictions
