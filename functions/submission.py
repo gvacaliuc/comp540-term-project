2252a640701676a7cbe5329d84e5ce00570ad059
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2018-03-16 12:44:13 -0400

    added rl encoding code

--- functions/submission.py
+++ functions/submission.py
@@ -5,11 +5,45 @@ Contains methods to process nuclei and individual nucleus predictions by
 performing the requisite RLE encoding.
 """
 
+import numpy as np
+
 def encode_rle_single_mask(nucleus_mask):
     """
     Run length encodes a single mask.
 
-    :param nucleus_mask: a ndarray masking a single nucleus in the image
-    :return encoding: a list of tuples holding the run length encoding of a 
-                      mask
+    :param nucleus_mask: a binary ndarray masking a single nucleus in the image
+    :return rle: a list of tuples holding the run length encoding of a 
+                 mask using a flattened 1-indexed coding
     """
+
+    flat_bw_mask = (nucleus_mask > 0).flatten()
+
+    nonzero_ind  = np.nonzero(flat_bw_mask)[0]
+    diff = np.diff(nonzero_ind)
+    runstarts = np.hstack([[0], np.nonzero(diff != 1)[0] + 1])
+    rle = [(1 + nonzero_ind[start], nextstart - start) 
+           for (start, nextstart) in zip(runstarts[:-1], runstarts[1:])]
+    last_start = runstarts[-1]
+    rle += [(1 + nonzero_ind[last_start], len(nonzero_ind) - last_start)]
+
+    return rle
+
+def decode_rle(encoding, shape):
+    """
+    Decodes a run-length encoding into a mask, given a shape.  Used to test
+    the encoding method.
+
+    :param encoding: a list of tuples holding the run-length encoding
+    :param shape: a tuple holding the height and with of our mask
+    :return mask: the rebuilt mask
+    """
+
+    mask = np.zeros_like(Y_train[0]).flatten()
+
+    for tup in rle_encoding:
+        true_ind = tup[0] - 1
+        mask[true_ind:(true_ind + tup[1])] = 1
+        
+    mask = mask.reshape(Y_train[0].shape)
+
+    return mask
