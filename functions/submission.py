49783eac1dc1395b584ed90829089bc799978bbe
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2018-03-18 13:55:26 -0400

    added RLEncoder to perform run length encoding

--- functions/submission.py
+++ functions/submission.py
@@ -6,6 +6,8 @@ performing the requisite RLE encoding.
 """
 
 import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator
 
 
 def encode_rle_single_mask(nucleus_mask):
@@ -31,6 +33,7 @@ def encode_rle_single_mask(nucleus_mask):
 
     return rle
 
+
 def decode_rle(encoding, shape):
     """
     Decodes a run-length encoding into a mask, given a shape.  Used to test
@@ -50,3 +53,50 @@ def decode_rle(encoding, shape):
     mask = mask.reshape(shape, order = "F")
 
     return mask
+
+
+def encode_image_masks(nuclei_masks):
+    """
+    Performs the run length encoding on each mask of an iterable.
+    """
+
+    return [encode_rle_single_mask(mask) for mask in nuclei_masks]
+
+
+class RLEncoder(BaseEstimator):
+    """
+    Class to run length encode a set of image predictions.
+    """
+
+    def __init__(self):
+        super()
+
+    def fit(self, metadata, predictions):
+        """
+        Fits the encoder with the given metadata and predicted binary masks.
+
+        :param metadata: a dataframe with two columns per image: image_id and
+                         orig_shape
+        :type metadata: pandas.DataFrame
+        :param predictions: a list of lists of binary 2D numpy arrays which
+                            have already been reshapen according to the
+                            original shape in the metadata.  Each sublist
+                            indicates a list of nuclei predictions.  Each array
+                            in the sublist is an individual nucleus mask.
+        :type predictions: iterable
+        """
+
+        if (len(metadata) != len(predictions)):
+            raise ValueError("""metadata and prediction list must be the same
+                    length.""")
+
+        encodings = [encode_image_masks(mask_list) for mask_list in predictions]
+
+        #   Turn encodings into strings
+        rle_list_to_str_list = lambda lst: [" ".join(["{} {}".format(*tup)
+                                                      for tup in code])
+                                            for code in lst]
+        encodings = [rle_list_to_str_list(rle_list) for rle_list in encodings]
+
+        self.encoding_ = metadata.copy()
+        self.encoding_["rle_mask_list"] = encodings
